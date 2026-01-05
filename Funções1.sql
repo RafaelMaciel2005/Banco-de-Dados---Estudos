1cfb88ad9d0dc3191622096f95a2e13b55c4cd59
Rafael Maciel <maciel.rafael2005@gmail.com>
2026-01-05 17:51:36 -0300

    Update Funções1.sql

--- Funções1.sql
+++ Funções1.sql
@@ -3,5 +3,8 @@ use sakila;
 select 
 	MAX(amount) AS "Valor Máximo",
     MIN(amount) AS "Valor Mínimo",
-    AVG(amount) AS Média
-from payment;		
+    AVG(amount) AS Média,
+    SUM(amount) AS "TOTAL DE VENDAS",
+    COUNT(AMOUNT) AS "NUMERO DE VENDAS"
+from payment
+WHERE STAFF_ID = 2;		
